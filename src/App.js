d218de45d3f8253fd3274d9b7cfc6e4a85dca43e
Rishabh Raghav <rishabhraghav2012@gmail.com>
2023-05-02 04:37:45 +0530

    added animations

--- src/App.js
+++ src/App.js
@@ -108,15 +108,15 @@ function App() {
 
   if(account == null) {
     return (
-      <div className="container">
+      <div className="container home">
         <br/>
         <div className="headings">
-        <h1>NFT MARKET PLACE</h1>
-        <h2>by Rishabh</h2>
+        <h1 className='grow-on-render'>NFT MARKET PLACE</h1>
+        <h2 className='grow-on-render'>by Rishabh</h2>
         <p>Buy an NFT from our market place</p>
         </div>
         {isWalletInstalled? 
-        (<button className="connectAccount" onClick={connectWallet}>Connect Wallet</button>) 
+        (<button className="connectAccount grow-on-render" onClick={connectWallet}>Connect Wallet</button>) 
         : (<p>Install MetaMask Wallet</p>) }
       </div>
     );
@@ -126,40 +126,40 @@ function App() {
   return (
     <div className="container">
       <br/>
-
+      
       <div className="headings">
-      <h1>Army of Demons</h1>
-      <h2>Kibutsuji</h2>
+      <h1 className='grow-on-render'>Army of Demons</h1>
+      <h2 className='grow-on-render'>Kibutsuji</h2>
       </div>
 
       <div className="nfts">
       {data.map((item, index) => (
-        <div className="imgDiv">
+        <div className="imgDiv grow-on-render">
           <img src={item.url} key={index} alt="img" width={250} height={250}/>
-          <button isloading={isMinting} onClick={() => {
+          <button className='grow-on-render widthNormal' isloading={isMinting} onClick={() => {
+            eval(item.param);
+          }} >Mint - 0.01 eth</button>
+        </div>
+      ))}
+      </div>
+  
+      <div className="headings">
+      <h2 className='grow-on-render'>female Kibutsuji</h2>
+      </div>
+      
+      <div className="nfts">
+      {data2.map((item, index) => (
+        <div className="imgDiv grow-on-render">
+          <img src={item.url} key={index} alt="img" width={250} height={250}/>
+          <button className='grow-on-render' isLoading={isMinting} onClick={() => {
             eval(item.param);
           }} >Mint - 0.01 eth</button>
         </div>
       ))}
       </div>
 
-      <div className="headings">
-      <h2>female Kibutsuji</h2>
-      </div>
-      
-      <div className="nfts">
-      {data2.map((item, index) => (
-        <div className="imgDiv">
-          <img src={item.url} key={index} alt="img" width={250} height={250}/>
-          <button isLoading={isMinting} onClick={() => {
-            eval(item.param);
-          }} >Mint - 0.01 eth</button>
-        </div>
-      ))}
-      </div>
-      
       <div className="withdraw">
-      <button onClick={withdrawMoney} >Withdraw Money from Contract</button>
+      <button className='grow-on-render widthNormal' onClick={withdrawMoney} >Withdraw Money from Contract</button>
       </div>
       
       
@@ -168,150 +168,3 @@ function App() {
 }
 
 export default App;
-// import "./App.css";
-// import { Contract, ethers } from "ethers";
-// import { useEffect, useState } from "react";
-// import contractABI from "./contractABI.json";
- 
-// const contractAddress = "<your_contract_address_here>";
- 
-// function App() {
- 
-//   const [account, setAccount] = useState(null);
-//   const [isWalletInstalled, setIsWalletInstalled] = useState(false);
-//   const [NFTContract, setNFTContract] = useState(null);
-//   // state for whether app is minting or not.
-//   const [isMinting, setIsMinting] = useState(false);
- 
-//   useEffect(() => {
-//     if (window.ethereum) {
-//       setIsWalletInstalled(true);
-//     }
-//   }, []);
- 
-//   useEffect(() => {
-//       function initNFTContract() {
-//         const provider = new ethers.providers.Web3Provider(window.ethereum);
-//         const signer = provider.getSigner();
-//         setNFTContract(new Contract(contractAddress,contractABI.abi,signer));
-//       }
-//       initNFTContract();
-//   }, [account]);
- 
- 
-//   async function connectWallet() {
-//     window.ethereum
-//       .request({
-//         method: "eth_requestAccounts",
-//       })
-//       .then((accounts) => {
-//         setAccount(accounts[0]);
-//       })
-//       .catch((error) => {
-//         alert("Something went wrong");
-//       });
-//   }
-
-//   const data = [
-//     {
-//       url: "./assets/images/1.png",
-//       param: "handleMint('<your_metadata_pinata_link_here>')",
-//     },
-//     {
-//       url: "./assets/images/2.png",
-//       param: "handleMint('<your_metadata_pinata_link_here>')",
-//     },
-//     {
-//       url: "./assets/images/3.png",
-//       param: "handleMint('<your_metadata_pinata_link_here>')",
-//     },
-//     {
-//       url: "./assets/images/4.png",
-//       param: "handleMint('<your_metadata_pinata_link_here>')",
-//     },
-//     {
-//       url: "./assets/images/5.png",
-//       param: "handleMint('<your_metadata_pinata_link_here>')",
-//     },
-//   ];
- 
-//   async function withdrawMoney(){
-//     try {
-//       const response = await NFTContract.withdrawMoney();
-//       console.log("Received: ", response);
-//     } catch (err) {
-//       alert(err);
-//     }
-//   }
- 
-//   async function handleMint(tokenURI) {
-//     setIsMinting(true);
-//       try {
-//         const options = {value: ethers.utils.parseEther("0.01")};
-//         const response = await NFTContract.mintNFT(tokenURI, options);
-//         console.log("Received: ", response);
-//       } catch (err) {
-//         alert(err);
-//       }
-//       finally {
-//         setIsMinting(false);
-//       }
-//   }
- 
-//   if (account === null) {
-//     return (
-//       <>
-//         <div className="container">
-//           <br/>
-//           <h1>🔮 metaschool</h1>
-//           <h2>NFT Marketplace</h2>
-//           <p>Buy an NFT from our marketplace.</p>
-
-//           {isWalletInstalled ? (
-//             <button onClick={connectWallet}>Connect Wallet</button>
-//           ) : (
-//             <p>Install Metamask wallet</p>
-//           )}
-//         </div>
-//       </>
-//     );
-//   }
- 
-//   return (
-//     <>
-//       <div className="container">
-//         <br/>
-//         <h1>🔮 metaschool</h1>
-      
-//         <h2>NFT Marketplace</h2>
-//           {data.map((item, index) => (
-//             <div className="imgDiv">
-//               <img
-//                 src={item.url}
-//                 key={index}
-//                 alt="images"
-//                 width={250}
-//                 height={250}
-//               />
-//               <button isLoading={isMinting}
-//                 onClick={() => {
-//                   eval(item.param);
-//                 }}
-//               >
-//                 Mint - 0.01 eth
-//               </button>
-//             </div>
-//           ))}
-//             <button 
-//               onClick={() => {
-//                 withdrawMoney();
-//               }}
-//             >
-//               Withdraw Money from Contract
-//             </button>
-//       </div>
-//     </>
-//   );
-// }
- 
-// export default App;
